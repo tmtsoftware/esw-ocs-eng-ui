01f0066b763ee8e7ac8bd5282050c2c2a5c62358
ManishGowardipe <gowardipemanish95@gmail.com>
2021-03-04 18:43:04 +0530

    ESW-445: Manish : Separate out test for SMActions

---
+++ test/containers/app/SmActions.unit.test.tsx
@@ -0,0 +1,81 @@
+import {
+  AgentStatus,
+  AgentStatusResponse,
+  ComponentId,
+  Prefix,
+  SequenceComponentStatus
+} from '@tmtsoftware/esw-ts'
+import { expect } from 'chai'
+import React from 'react'
+import { mock, verify, when } from 'ts-mockito'
+import SmActions from '../../../src/containers/infrastructure/SMActions'
+import {
+  getMockServices,
+  renderWithAuth,
+  screen,
+  waitFor
+} from '../../utils/test-utils'
+describe('SM actions', () => {
+  const mockServices = getMockServices()
+  const smService = mockServices.mock.smService
+  const sequenceComponentStatus = mock<SequenceComponentStatus>()
+
+  const agentStatus: AgentStatus = {
+    agentId: new ComponentId(new Prefix('APS', 'jdks'), 'Machine'),
+    seqCompsStatus: [sequenceComponentStatus]
+  }
+
+  const agentStatusSuccess1: AgentStatusResponse = {
+    _type: 'Success',
+    agentStatus: [agentStatus],
+    seqCompsWithoutAgent: []
+  }
+
+  const agentStatusSuccess2: AgentStatusResponse = {
+    _type: 'Success',
+    agentStatus: [],
+    seqCompsWithoutAgent: [sequenceComponentStatus]
+  }
+
+  const unProvisionRenderTestData: [AgentStatusResponse, string][] = [
+    [agentStatusSuccess1, 'on any agent'],
+    [agentStatusSuccess2, 'without any agent']
+  ]
+  unProvisionRenderTestData.map(([agentStatusResponse, name]) => {
+    it(`should render provision button as enabled & configure button as disabled if no seq comps are running ${name} | ESW-442, ESW-445`, async () => {
+      when(smService.getAgentStatus()).thenResolve(agentStatusResponse)
+
+      renderWithAuth({
+        ui: <SmActions />,
+        loggedIn: true,
+        mockClients: mockServices.serviceFactoryContext
+      })
+
+      expect(
+        (screen.getByRole('button', { name: 'Configure' }) as HTMLButtonElement)
+          .disabled
+      ).true
+      expect(
+        (screen.getByRole('button', { name: 'Provision' }) as HTMLButtonElement)
+          .disabled
+      ).true
+      // wait for both buttons to be enabled once data is loaded in async manner
+      await waitFor(() => {
+        expect(
+          (screen.getByRole('button', {
+            name: 'Provision'
+          }) as HTMLButtonElement).disabled
+        ).false
+      })
+      await waitFor(() => {
+        expect(
+          (screen.getByRole('button', {
+            name: 'Configure'
+          }) as HTMLButtonElement).disabled
+        ).false
+      })
+
+      verify(smService.getAgentStatus()).called()
+    })
+  })
+})

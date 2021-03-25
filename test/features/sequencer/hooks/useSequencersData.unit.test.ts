a77c78cab0d4fbad99b5f9cfd3ef31bcc932cafe
anujachaitanya <anujachaitanya09@gmail.com>
2021-03-25 09:29:13 +0530

    #000 : Anuja : Add tests for useSequencersData

---
+++ test/features/sequencer/hooks/useSequencersData.unit.test.ts
@@ -0,0 +1,137 @@
+import { renderHook } from '@testing-library/react-hooks/dom'
+import { Prefix, Setup, Step } from '@tmtsoftware/esw-ts'
+import { expect } from 'chai'
+import { mock, verify, when } from 'ts-mockito'
+import { useSequencersData } from '../../../../src/features/sequencer/hooks/useSequencersData'
+import {
+  getContextWithQueryClientProvider,
+  getMockServices
+} from '../../../utils/test-utils'
+
+describe('useSequencersData', () => {
+  const step = (
+    stepStatus: 'Pending' | 'Success' | 'Failure' | 'InFlight',
+    hasBreakpoint = false
+  ): Step => {
+    return {
+      hasBreakpoint: hasBreakpoint,
+      status: { _type: stepStatus, message: '' },
+      command: mock(Setup),
+      id: ''
+    }
+  }
+
+  const stepList1: Step[] = [step('Pending', true), step('Pending')]
+  const stepList2: Step[] = [step('Success'), step('Failure'), step('Pending')]
+  const stepList3: Step[] = [step('InFlight'), step('Pending')]
+  const stepList4: Step[] = [step('Success'), step('Success')]
+
+  it('should return sequencers data with their steps and status | ESW-451', async () => {
+    const mockServices = getMockServices()
+    const sequencerService = mockServices.mock.sequencerService
+
+    when(sequencerService.getSequence())
+      .thenResolve(stepList1)
+      .thenResolve(stepList2)
+      .thenResolve(stepList3)
+      .thenResolve(stepList4)
+
+    const ContextAndQueryClientProvider = getContextWithQueryClientProvider(
+      true,
+      mockServices.serviceFactoryContext
+    )
+
+    const { result, waitFor } = renderHook(
+      () =>
+        useSequencersData([
+          Prefix.fromString('ESW'),
+          Prefix.fromString('WFOS.Calib'),
+          Prefix.fromString('IRIS.FilterWheel'),
+          Prefix.fromString('IRIS.Darknight')
+        ]),
+      {
+        wrapper: ContextAndQueryClientProvider
+      }
+    )
+
+    await waitFor(() => {
+      return result.current.isSuccess
+    })
+
+    verify(sequencerService.getSequence()).called()
+
+    expect(result.current.data).to.deep.equal([
+      {
+        location: null,
+        prefix: 'ESW.',
+        status: {
+          status: 'Paused',
+          stepNumber: 1
+        },
+        totalSteps: 2
+      },
+      {
+        location: null,
+        prefix: 'WFOS.Calib',
+        status: {
+          status: 'Failed',
+          stepNumber: 2
+        },
+        totalSteps: 3
+      },
+      {
+        location: null,
+        prefix: 'IRIS.FilterWheel',
+        status: {
+          status: 'In Progress',
+          stepNumber: 1
+        },
+        totalSteps: 2
+      },
+      {
+        location: null,
+        prefix: 'IRIS.Darknight',
+        status: {
+          status: 'All Steps Completed',
+          stepNumber: 0
+        },
+        totalSteps: 2
+      }
+    ])
+  }),
+    it('should return Failed to fetch data if error occurred | ESW-451', async () => {
+      const mockServices = getMockServices()
+      const sequencerService = mockServices.mock.sequencerService
+
+      when(sequencerService.getSequence()).thenReject(Error())
+
+      const ContextAndQueryClientProvider = getContextWithQueryClientProvider(
+        true,
+        mockServices.serviceFactoryContext
+      )
+
+      const { result, waitFor } = renderHook(
+        () => useSequencersData([Prefix.fromString('IRIS.Darknight')]),
+        {
+          wrapper: ContextAndQueryClientProvider
+        }
+      )
+
+      await waitFor(() => {
+        return result.current.isSuccess
+      })
+
+      verify(sequencerService.getSequence()).called()
+
+      expect(result.current.data).to.deep.equal([
+        {
+          prefix: 'IRIS.Darknight',
+          status: {
+            status: 'Failed to Fetch Status',
+            stepNumber: 0
+          },
+          totalSteps: 'NA'
+        }
+      ])
+    })
+})

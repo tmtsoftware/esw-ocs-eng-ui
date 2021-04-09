19e4d4cfa2387ad37a546a3ce655b670d75119a6
Manish <manish.gowardipe@thoughtworks.com>
2021-04-09 13:11:07 +0530

    #ESW-000: Manish : Remove duplicate foders

--- src/components/Table/HeaderTitle.tsx
+++
@@ -1,8 +0,0 @@
-import { Typography } from 'antd'
-import React from 'react'
-
-export const HeaderTitle = ({ title }: { title: string }): JSX.Element => (
-  <Typography.Title level={5} style={{ marginBottom: 0 }}>
-    {title}
-  </Typography.Title>
-)

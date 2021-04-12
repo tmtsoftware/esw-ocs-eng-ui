d6f2d4aff0b7ad8157e757ed6bb88173ffc72b07
Manish <manish.gowardipe@thoughtworks.com>
2021-04-12 19:51:21 +0530

    #000: Manish: Handle laoding state properly

--- src/features/sm/hooks/useSMStatus.ts
+++ src/features/sm/hooks/useSMStatus.ts
@@ -11,20 +11,18 @@ export const useSMStatus = (
   locationService: LocationService
 ): SMContextType => {
   const [location, setLocation] = useState<Location | undefined>(undefined)
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const callback = async (trackingEvent: TrackingEvent) => {
-      setLoading(false)
       if (trackingEvent._type === 'LocationRemoved') {
         setLocation(undefined)
       } else {
         setLocation(trackingEvent.location)
       }
     }
-    setLoading(true)
     const sub = locationService.track(SM_CONNECTION)(callback)
-
+    setLoading(false)
     return sub.cancel
   }, [locationService])
 

9f3cadfc7804e4f05eabdebf157e532efcae9c6b
anujachaitanya <anujachaitanya09@gmail.com>
2021-04-15 15:03:37 +0530

    #ESW-490 : Anuja | Poorva : Add test for handleMenuClick in stepActions

--- test/features/sequencer/components/sequencerDetails/StepActions.unit.test.tsx
+++ test/features/sequencer/components/sequencerDetails/StepActions.unit.test.tsx
@@ -1,27 +1,30 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Prefix, Setup, Step } from '@tmtsoftware/esw-ts'
+import { expect } from 'chai'
 import React from 'react'
+import { when } from 'ts-mockito'
 import { StepActions } from '../../../../../src/features/sequencer/components/sequencerDetails/StepActions'
 import { getMockServices, renderWithAuth } from '../../../../utils/test-utils'
 
 describe('StepActions', () => {
   const mockServices = getMockServices()
-
+  const sequencerService = mockServices.mock.sequencerService
   const sequencerPrefix = Prefix.fromString('ESW.iris_darknight')
 
-  it('should give insert breakpoint menu if step does not have breakpoint | ESW-459', async () => {
-    const step: Step = {
-      hasBreakpoint: false,
-      status: { _type: 'Success' },
-      command: new Setup(Prefix.fromString('ESW.test'), 'Command-1'),
-      id: 'step1'
-    }
+  const getStepWithBreakpoint = (hasBreakpoint: boolean): Step => ({
+    hasBreakpoint: hasBreakpoint,
+    status: { _type: 'Success' },
+    command: new Setup(Prefix.fromString('ESW.test'), 'Command-1'),
+    id: 'step1'
+  })
 
+  it('should give insert breakpoint menu if step does not have breakpoint | ESW-459', async () => {
     renderWithAuth({
       ui: (
         <StepActions
           sequencerPrefix={sequencerPrefix}
-          step={step}
+          step={getStepWithBreakpoint(false)}
           handleMenuClick={() => ({})}
         />
       ),
@@ -32,18 +35,11 @@ describe('StepActions', () => {
   })
 
   it('should give remove breakpoint menu if step has breakpoint | ESW-459', async () => {
-    const step: Step = {
-      hasBreakpoint: true,
-      status: { _type: 'Success' },
-      command: new Setup(Prefix.fromString('ESW.test'), 'Command-1'),
-      id: 'step1'
-    }
-
     renderWithAuth({
       ui: (
         <StepActions
           sequencerPrefix={sequencerPrefix}
-          step={step}
+          step={getStepWithBreakpoint(true)}
           handleMenuClick={() => ({})}
         />
       ),
@@ -51,5 +47,31 @@ describe('StepActions', () => {
     })
 
     await screen.findByText('Remove breakpoint')
+  })
+
+  it('should call handleMenuClick on click | ESW-490', async () => {
+    let called = false
+    const handleMenuClick = () => {
+      called = !called
+    }
+
+    when(sequencerService.removeBreakpoint('step1')).thenResolve({
+      _type: 'Ok'
+    })
+    renderWithAuth({
+      ui: (
+        <StepActions
+          sequencerPrefix={sequencerPrefix}
+          step={getStepWithBreakpoint(true)}
+          handleMenuClick={handleMenuClick}
+        />
+      ),
+      mockClients: mockServices.serviceFactoryContext
+    })
+
+    const removeAction = await screen.findByText('Remove breakpoint')
+    userEvent.click(removeAction, { button: 0 })
+
+    expect(called).to.be.true
   })
 })

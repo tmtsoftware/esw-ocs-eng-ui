280ee0688bf29f4552487383fbf40a0eea162efc
Manish <manish.gowardipe@thoughtworks.com>
2021-04-26 19:13:34 +0530

    #000 : Anuja | Manish : Remove back button from all pages

--- src/components/pageHeader/PageHeader.tsx
+++ src/components/pageHeader/PageHeader.tsx
@@ -1,19 +1,13 @@
 import { PageHeader as AntPageHeader, PageHeaderProps } from 'antd'
 import React, { ReactNode } from 'react'
-import { useHistory } from 'react-router'
 import styles from './pageHeader.module.css'
 
 export const PageHeader = ({
   title,
   ...props
 }: PageHeaderProps & { children?: ReactNode }): JSX.Element => {
-  const history = useHistory()
   return (
-    <AntPageHeader
-      onBack={() => history.goBack()}
-      className={styles.pageHeader}
-      title={title}
-      {...props}>
+    <AntPageHeader className={styles.pageHeader} title={title} {...props}>
       {props.children}
     </AntPageHeader>
   )

230081b2610647e97e1822a3b744bf3b5a92596e
Shubham Jaybhaye <shubhamj7997@gmail.com>
2021-05-13 15:32:51 +0530

    #ESW-461 : Anuja | Shubham : Add test for error cases for steps component. Change severity of unused variables to "warn" in eslint

--- test/features/sequencer/components/sequencerDetails/AddSteps.unit.test.tsx
+++ test/features/sequencer/components/sequencerDetails/AddSteps.unit.test.tsx
@@ -1,39 +1,87 @@
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Prefix, SequenceCommand, Setup } from '@tmtsoftware/esw-ts'
+import {
+  GenericResponse,
+  Prefix,
+  SequenceCommand,
+  Setup
+} from '@tmtsoftware/esw-ts'
 import React from 'react'
 import { anything, deepEqual, verify, when } from 'ts-mockito'
 import { AddSteps } from '../../../../../src/features/sequencer/components/sequencerDetails/AddSteps'
+import {
+  cannotOperateOnAnInFlightOrFinishedStepMsg,
+  idDoesNotExistMsg
+} from '../../../../../src/features/sequencer/components/sequencerResponsesMapping'
 import {
   renderWithAuth,
   sequencerServiceMock
 } from '../../../../utils/test-utils'
 
+type TestData = {
+  testName: string
+  response: GenericResponse
+  message: string
+}
+
 describe('AddSteps', () => {
-  it('should add uploaded steps', async () => {
-    const id = 'step_1'
-    const seqPrefix = Prefix.fromString('ESW.darknight')
-    const commands: SequenceCommand[] = [
-      new Setup(Prefix.fromString('CSW.filter'), 'move', [], '2020A-001-123')
-    ]
-    const file = new File([JSON.stringify({ commands })], 'commands.json')
+  const id = 'step_1'
+  const seqPrefix = Prefix.fromString('ESW.darknight')
+  const commands: SequenceCommand[] = [
+    new Setup(Prefix.fromString('CSW.filter'), 'move', [], '2020A-001-123')
+  ]
+  const unhandledMsg = 'unhandled'
 
-    when(sequencerServiceMock.insertAfter(id, anything())).thenResolve({
-      _type: 'Ok'
+  const file = new File([JSON.stringify({ commands })], 'commands.json')
+  const testCases: TestData[] = [
+    {
+      testName: 'should add uploaded steps',
+      response: { _type: 'Ok' },
+      message: 'Successfully added steps'
+    },
+    {
+      testName: 'should show error if step id does not exist',
+      response: { _type: 'IdDoesNotExist', id },
+      message: `Failed to add steps, reason: ${idDoesNotExistMsg(id)}`
+    },
+    {
+      testName: 'should show error if the step is finished',
+      response: { _type: 'CannotOperateOnAnInFlightOrFinishedStep' },
+      message: `Failed to add steps, reason: ${cannotOperateOnAnInFlightOrFinishedStepMsg}`
+    },
+    {
+      testName: 'should show error if unhandled response is given by sequencer',
+      response: {
+        _type: 'Unhandled',
+        msg: unhandledMsg,
+        state: 'idle',
+        messageType: 'InsertAfter'
+      },
+      message: `Failed to add steps, reason: ${unhandledMsg}`
+    }
+  ]
+
+  testCases.forEach(({ testName, response, message }) => {
+    it(testName, async () => {
+      when(sequencerServiceMock.insertAfter(id, anything())).thenResolve(
+        response
+      )
+
+      renderWithAuth({
+        ui: (
+          <AddSteps disabled={false} sequencerPrefix={seqPrefix} stepId={id} />
+        )
+      })
+
+      const upload = await screen.findByRole('button', { name: /add steps/i })
+      userEvent.click(upload)
+
+      // eslint-disable-next-line testing-library/no-node-access
+      const inputBox = upload.firstChild as HTMLInputElement
+      userEvent.upload(inputBox, file)
+
+      await screen.findByText(message)
+      verify(sequencerServiceMock.insertAfter(id, deepEqual(commands))).called()
     })
-
-    renderWithAuth({
-      ui: <AddSteps disabled={false} sequencerPrefix={seqPrefix} stepId={id} />
-    })
-
-    const upload = await screen.findByRole('button', { name: /add steps/i })
-    userEvent.click(upload)
-
-    // eslint-disable-next-line testing-library/no-node-access
-    const inputBox = upload.firstChild as HTMLInputElement
-    userEvent.upload(inputBox, file)
-
-    await screen.findByText('Successfully added steps')
-    verify(sequencerServiceMock.insertAfter(id, deepEqual(commands))).called()
   })
 })

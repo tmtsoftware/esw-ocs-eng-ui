958695ac3fd7b42406e95de48dc7984189f8aa7d
Shivani Thorat <shivanimt31@gmail.com>
2021-05-31 15:32:14 +0530

    #ESW-502: Shivani: Add styling to DisbledSequencerActions

--- src/features/agent/components/SequenceComponentActions.tsx
+++ src/features/agent/components/SequenceComponentActions.tsx
@@ -1,15 +1,18 @@
 import { MoreOutlined } from '@ant-design/icons'
 import type { ComponentId } from '@tmtsoftware/esw-ts'
 import { Dropdown, Menu } from 'antd'
+import useBreakpoint from 'antd/es/grid/hooks/useBreakpoint'
 import React from 'react'
 import { ReloadScript } from '../../sm/components/ReloadScript'
 import { KillSequenceComponent } from './KillSequenceComponent'
 
 const DisabledSequencerActions = () => {
+  const screen = useBreakpoint()
+  const width = screen.lg ? '200px' : '150px'
   const displayMessage = 'Spawned sequencers will display more actions'
   return (
-    <Menu.Item key='disabledSequencerActions' disabled={true} icon={<></>}>
-      {displayMessage}
+    <Menu.Item key='disabledSequencerActions' disabled={true}>
+      <div style={{ maxWidth: width, whiteSpace: 'pre-wrap', fontStyle: 'italic' }}>{displayMessage}</div>
     </Menu.Item>
   )
 }

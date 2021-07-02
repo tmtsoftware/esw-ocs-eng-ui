5a238ff2e23e38c5cf7cbf6d7cc74eca25d04f2d
anujachaitanya <anujachaitanya09@gmail.com>
2021-07-02 16:51:08 +0530

    #ESW-503 : Anuja | Manish : Use latest esw-ts and minor refactor

--- test/features/sequencer/components/sequencerDetails/ParamFormatter.unit.test.tsx
+++ test/features/sequencer/components/sequencerDetails/ParamFormatter.unit.test.tsx
@@ -19,9 +19,9 @@ import {
   solarSystemCoordKey,
   Tag,
   taiTimeKey,
-  utcTimeKey
+  utcTimeKey,
+  Angle
 } from '@tmtsoftware/esw-ts'
-import { Angle } from '@tmtsoftware/esw-ts/dist/src/models/params/Angle'
 import { expect } from 'chai'
 import React from 'react'
 import { formatParameters } from '../../../../../src/features/sequencer/components/sequencerDetails/ParamFormatter'
@@ -70,24 +70,24 @@ describe('Parameter Formatter', () => {
   const setupWithIntKey = new Setup(Prefix.fromString('ESW.iris_darknight'), 'intKey', [intParam])
 
   //expectations
-  const formatedEqCoord = `Base: RA=0.0000034280555555555554 DEC=0.0000034280555555555554\nICRS, Catalog="cat123"\nProper Motion=123 123414`
-  const formatedCometCoord = `Base: Epoch of Perihelion=11\ninclination=0.0000034280555555555554 degrees\nLong Ascending Node=0.0000034280555555555554 degrees\nArgument of Perihelion=0.0000034280555555555554 degrees\nPerihelion Distance=200 AU\nEccentricity=0.4\n`
-  const formatedMinorCoord = `Base: Epoch=2\ninclination=0.0000034280555555555554 degrees\nLong Ascending Node=0.0000034280555555555554 degrees\nArgument of Perihelion=0.0000034280555555555554 degrees\nMean Distance=2 AU\nEccentricity=2\nMean Anomaly=0.0000034280555555555554 degrees\n`
-  const formatedSolarSystemCoord = `Base: Jupiter`
-  const formatedAltAzCoord = `Base: Alt=0.0000034280555555555554 Az=0.0000034280555555555554`
+  const formattedEqCoord = `Base: RA=0.0000034280555555555554 DEC=0.0000034280555555555554\nICRS, Catalog="cat123"\nProper Motion=123 123414`
+  const formattedCometCoord = `Base: Epoch of Perihelion=11\ninclination=0.0000034280555555555554 degrees\nLong Ascending Node=0.0000034280555555555554 degrees\nArgument of Perihelion=0.0000034280555555555554 degrees\nPerihelion Distance=200 AU\nEccentricity=0.4\n`
+  const formattedMinorCoord = `Base: Epoch=2\ninclination=0.0000034280555555555554 degrees\nLong Ascending Node=0.0000034280555555555554 degrees\nArgument of Perihelion=0.0000034280555555555554 degrees\nMean Distance=2 AU\nEccentricity=2\nMean Anomaly=0.0000034280555555555554 degrees\n`
+  const formattedSolarSystemCoord = `Base: Jupiter`
+  const formattedAltAzCoord = `Base: Alt=0.0000034280555555555554 Az=0.0000034280555555555554`
   const formattedUtcTime = '1970-01-01T00:00:00Z'
   const formattedTaiTime = '2017-09-04T19:00:00.123456789Z'
-  const unFormattedIntValue = '123, 12432'
+  const formattedIntValue = '123, 12432'
 
   const testCases: [Parameter<Key>, SequenceCommand, string][] = [
-    [eqCoord, setupWithEqCoord, formatedEqCoord],
-    [cometCoord, setupWithCometCoord, formatedCometCoord],
-    [minorCoord, setupWithMinorPlanetCoord, formatedMinorCoord],
-    [solarSystemCoord, setupWithSolarSystemCoord, formatedSolarSystemCoord],
-    [altAzCoord, setupWithAltAzCoord, formatedAltAzCoord],
+    [eqCoord, setupWithEqCoord, formattedEqCoord],
+    [cometCoord, setupWithCometCoord, formattedCometCoord],
+    [minorCoord, setupWithMinorPlanetCoord, formattedMinorCoord],
+    [solarSystemCoord, setupWithSolarSystemCoord, formattedSolarSystemCoord],
+    [altAzCoord, setupWithAltAzCoord, formattedAltAzCoord],
     [utcTime, setupWithUtcTimeKey, formattedUtcTime],
     [taiTime, setupWithTaiTimeKey, formattedTaiTime],
-    [intParam, setupWithIntKey, unFormattedIntValue]
+    [intParam, setupWithIntKey, formattedIntValue]
   ]
 
   testCases.forEach(([parameter, command, formattedParam]) => {

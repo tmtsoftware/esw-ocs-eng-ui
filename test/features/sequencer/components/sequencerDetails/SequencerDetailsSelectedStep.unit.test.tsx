e62e517bc6134bd9da8f3b9d0bba4a898e96e75b
Manish <manish.gowardipe@thoughtworks.com>
2021-07-05 12:50:18 +0530

    #ESW-000 : Anuja | Manish : add delay in between assertions

--- test/features/sequencer/components/sequencerDetails/SequencerDetailsSelectedStep.unit.test.tsx
+++ test/features/sequencer/components/sequencerDetails/SequencerDetailsSelectedStep.unit.test.tsx
@@ -60,10 +60,9 @@ describe('sequencer details selected step', () => {
     // step1 in executng, ui should show step1 details on right side
     await assertRunningStepIs(/Command-1/i, 500)
     const sourceValue = screen.getByLabelText('Source-Value')
-    // eslint-disable-next-line testing-library/no-debug
-    screen.debug(sourceValue)
     expect(sourceValue.innerHTML).to.equals('ESW.test1')
 
+    await delay(10)
     //After some time , a new event is received, step2 in executng, ui should show step2 details on right side
     await assertRunningStepIs(/Command-2/i, 1200)
     await screen.findByText('ESW.test2')
@@ -143,10 +142,11 @@ describe('sequencer details selected step', () => {
     //step1 is executing, ui should show step3(which was clicked by user) details on right side
     await assertRunningStepIs(/Command-1/i, 500)
     await screen.findByText('ESW.test3')
-
+    await delay(10)
     //step2 is executing
     await assertRunningStepIs(/Command-2/i, 500)
 
+    await delay(10)
     //ui should show step2 details on right side as step3 got removed
     await screen.findByText('ESW.test2')
   })
@@ -196,16 +196,16 @@ describe('sequencer details selected step', () => {
     //step1 is executing, ui should show step3 (which was clicked by user) details on right side i.e. user goes to non-follow mode
     await assertRunningStepIs(/Command-1/i, 500)
     await screen.findByText('ESW.test3')
-
+    await delay(10)
     //step2 is executing, ui should show step3 (which was clicked by user) details on right side i.e. user is still in non-follow mode
     await assertRunningStepIs(/Command-2/i, 500)
     await screen.findByText('ESW.test3')
-
+    await delay(10)
     //user clicks step2, which is in progress to go in follow mode again, and now ui should show step2 details on right side
     const step2 = await screen.findByRole('button', { name: /Command-2/i })
     userEvent.click(step2)
     await screen.findByText('ESW.test2')
-
+    await delay(10)
     //as user is in follow mode, and after some time ui should show step3 details on right side as steplist progress
     await screen.findByText('ESW.test3')
   })
@@ -261,4 +261,4 @@ const assertRunningStepIs = async (step: RegExp, timeout: number) => {
   await waitFor(() => expect(stepButton1.style.borderColor).to.equal('rgb(82, 196, 26)'), { timeout })
 }
 
-export const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+export const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms))

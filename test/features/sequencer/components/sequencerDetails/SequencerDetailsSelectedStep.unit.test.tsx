f3df44eb53816e9a4a564ae12d31cfafbaad764f
Manish <manish.gowardipe@thoughtworks.com>
2021-07-05 14:50:03 +0530

    #ESW-000 : Anuja | Manish : Use window.postMessage to trigger subscribeSequencerState

--- test/features/sequencer/components/sequencerDetails/SequencerDetailsSelectedStep.unit.test.tsx
+++ test/features/sequencer/components/sequencerDetails/SequencerDetailsSelectedStep.unit.test.tsx
@@ -1,6 +1,6 @@
 import { screen, waitFor, within } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Location, Prefix, SequencerStateResponse, StepList } from '@tmtsoftware/esw-ts'
+import { Location, Prefix, SequencerStateResponse, StepList, StepStatus } from '@tmtsoftware/esw-ts'
 import { expect } from 'chai'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
@@ -12,6 +12,12 @@ import { mockServices, renderWithAuth, sendEvent, sequencerServiceMock } from '.
 describe('sequencer details selected step', () => {
   beforeEach(() => {
     reset(sequencerServiceMock)
+    when(sequencerServiceMock.subscribeSequencerState()).thenReturn((callback) => {
+      addEventListenerForSubscribing(callback)
+      return {
+        cancel: () => undefined
+      }
+    })
   })
   afterEach(() => {
     reset(sequencerServiceMock)
@@ -42,57 +48,43 @@ describe('sequencer details selected step', () => {
   })
 
   it('should follow step list progress by default when no user action | ESW-501, ESW-489', async () => {
-    const stepListWithStep1InProgress: StepList = new StepList([getStep('InFlight', '1'), getStep('Pending', '2')])
-
-    const stepListWithStep2InProgress: StepList = new StepList([getStep('Success', '1'), getStep('InFlight', '2')])
-
-    when(sequencerServiceMock.subscribeSequencerState()).thenReturn((callback) => {
-      sendEvent(callback, 'Running', stepListWithStep1InProgress)
-      sendEvent(callback, 'Running', stepListWithStep2InProgress, 700)
-      return {
-        cancel: () => undefined
-      }
-    })
+    const stepListWithStep1InProgress: StepStatus['_type'][] = ['InFlight', 'Pending']
+    const stepListWithStep2InProgress: StepStatus['_type'][] = ['Success', 'InFlight']
 
     renderWithAuth({
-      ui: <SequencerDetails prefix={sequencerLoc.connection.prefix} />
+      ui: (
+        <BrowserRouter>
+          <SequencerDetails prefix={sequencerLoc.connection.prefix} />
+        </BrowserRouter>
+      )
     })
+    //simulating backend event
+    window.postMessage(stepListWithStep1InProgress, '*')
     // step1 in executng, ui should show step1 details on right side
     await assertRunningStepIs(/Command-1/i, 500)
     const sourceValue = screen.getByLabelText('Source-Value')
     expect(sourceValue.innerHTML).to.equals('ESW.test1')
 
-    await delay(10)
+    //simulating backend event
+    window.postMessage(stepListWithStep2InProgress, '*')
     //After some time , a new event is received, step2 in executng, ui should show step2 details on right side
     await assertRunningStepIs(/Command-2/i, 1200)
     await screen.findByText('ESW.test2')
   })
 
   it('should not follow step list progress when user selects step other than in-flight step | ESW-501, ESW-489', async () => {
-    const stepListWithStep1InProgress: StepList = new StepList([
-      getStep('InFlight', '1'),
-      getStep('Pending', '2'),
-      getStep('Pending', '3')
-    ])
+    const stepListWithStep1InProgress: StepStatus['_type'][] = ['InFlight', 'Pending', 'Pending']
 
-    const stepListWithStep2InProgress: StepList = new StepList([
-      getStep('Success', '1'),
-      getStep('InFlight', '2'),
-      getStep('Pending', '3')
-    ])
-
-    when(sequencerServiceMock.subscribeSequencerState()).thenReturn((callback) => {
-      sendEvent(callback, 'Running', stepListWithStep1InProgress)
-      sendEvent(callback, 'Running', stepListWithStep2InProgress, 400)
-      return {
-        cancel: () => undefined
-      }
-    })
+    const stepListWithStep2InProgress: StepStatus['_type'][] = ['Success', 'InFlight', 'Pending']
 
     renderWithAuth({
-      ui: <SequencerDetails prefix={sequencerLoc.connection.prefix} />
+      ui: (
+        <BrowserRouter>
+          <SequencerDetails prefix={sequencerLoc.connection.prefix} />
+        </BrowserRouter>
+      )
     })
-
+    window.postMessage(stepListWithStep1InProgress, '*')
     //User clicks step3
     const step3 = await screen.findByRole('button', { name: /Command-3/i })
     userEvent.click(step3)
@@ -100,41 +92,26 @@ describe('sequencer details selected step', () => {
     //step1 is executing, but ui should show step3(which was clicked by user) details on right side
     await assertRunningStepIs(/Command-1/i, 500)
     await screen.findByText('ESW.test3')
-    await delay(10)
+    window.postMessage(stepListWithStep2InProgress, '*')
     //step2 is executing, ui should continue to show step3(which was clicked by user) details on right side
     await assertRunningStepIs(/Command-2/i, 500)
     await screen.findByText('ESW.test3')
   })
 
   it('should go to last existing step in stepList when user do abort sequence | ESW-501, ESW-489', async () => {
-    const stepListWithStep1InProgress: StepList = new StepList([
-      getStep('InFlight', '1'),
-      getStep('Pending', '2'),
-      getStep('Pending', '3')
-    ])
-
-    const stepListWithStep2InProgress: StepList = new StepList([
-      getStep('Success', '1'),
-      getStep('InFlight', '2'),
-      getStep('Pending', '3')
-    ])
-
+    const stepListWithStep1InProgress: StepStatus['_type'][] = ['InFlight', 'Pending', 'Pending']
+    const stepListWithStep2InProgress: StepStatus['_type'][] = ['Success', 'InFlight', 'Pending']
     //this simulates abort sequence action, and removes step3 from stepList which was not executed
-    const stepListWithStep3Removed: StepList = new StepList([getStep('Success', '1'), getStep('Success', '2')])
-
-    when(sequencerServiceMock.subscribeSequencerState()).thenReturn((callback) => {
-      sendEvent(callback, 'Running', stepListWithStep1InProgress)
-      sendEvent(callback, 'Running', stepListWithStep2InProgress, 400)
-      sendEvent(callback, 'Running', stepListWithStep3Removed, 1000)
-      return {
-        cancel: () => undefined
-      }
-    })
+    const stepListWithStep3Removed: StepStatus['_type'][] = ['Success', 'Success']
 
     renderWithAuth({
-      ui: <SequencerDetails prefix={sequencerLoc.connection.prefix} />
+      ui: (
+        <BrowserRouter>
+          <SequencerDetails prefix={sequencerLoc.connection.prefix} />
+        </BrowserRouter>
+      )
     })
-
+    window.postMessage(stepListWithStep1InProgress, '*')
     //user clicks step3
     const step3 = await screen.findByRole('button', { name: /Command-3/i })
     userEvent.click(step3)
@@ -142,42 +119,18 @@ describe('sequencer details selected step', () => {
     //step1 is executing, ui should show step3(which was clicked by user) details on right side
     await assertRunningStepIs(/Command-1/i, 500)
     await screen.findByText('ESW.test3')
+    window.postMessage(stepListWithStep2InProgress, '*')
     //step2 is executing
     await assertRunningStepIs(/Command-2/i, 1000)
-
+    window.postMessage(stepListWithStep3Removed, '*')
     //ui should show step2 details on right side as step3 got removed
     await screen.findByText('ESW.test2')
   })
 
   it('should start following step list progress again when user selects in-flight step | ESW-501', async () => {
-    const stepList: StepList = new StepList([
-      getStep('InFlight', '1'),
-      getStep('Pending', '2'),
-      getStep('Pending', '3')
-    ])
-
-    const updatedStepListWithStep2InProgress: StepList = new StepList([
-      getStep('Success', '1'),
-      getStep('InFlight', '2'),
-      getStep('Pending', '3')
-    ])
-
-    const updatedStepListWithStep3InProgress: StepList = new StepList([
-      getStep('Success', '1'),
-      getStep('Success', '2'),
-      getStep('InFlight', '3')
-    ])
-
-    when(sequencerServiceMock.subscribeSequencerState()).thenReturn(
-      (callback: (sequencerStateRes: SequencerStateResponse) => void) => {
-        sendEvent(callback, 'Running', stepList)
-        sendEvent(callback, 'Running', updatedStepListWithStep2InProgress, 400)
-        sendEvent(callback, 'Running', updatedStepListWithStep3InProgress, 800)
-        return {
-          cancel: () => undefined
-        }
-      }
-    )
+    const stepList: StepStatus['_type'][] = ['InFlight', 'Pending', 'Pending']
+    const updatedStepListWithStep2InProgress: StepStatus['_type'][] = ['Success', 'InFlight', 'Pending']
+    const updatedStepListWithStep3InProgress: StepStatus['_type'][] = ['Success', 'Success', 'InFlight']
 
     renderWithAuth({
       ui: (
@@ -186,7 +139,7 @@ describe('sequencer details selected step', () => {
         </BrowserRouter>
       )
     })
-
+    window.postMessage(stepList, '*')
     //user clicks step3
     const step3 = await screen.findByRole('button', { name: /Command-3/i })
     userEvent.click(step3)
@@ -194,46 +147,25 @@ describe('sequencer details selected step', () => {
     //step1 is executing, ui should show step3 (which was clicked by user) details on right side i.e. user goes to non-follow mode
     await assertRunningStepIs(/Command-1/i, 500)
     await screen.findByText('ESW.test3')
-    await delay(10)
+    window.postMessage(updatedStepListWithStep2InProgress, '*')
     //step2 is executing, ui should show step3 (which was clicked by user) details on right side i.e. user is still in non-follow mode
     await assertRunningStepIs(/Command-2/i, 500)
     await screen.findByText('ESW.test3')
-    await delay(10)
+
     //user clicks step2, which is in progress to go in follow mode again, and now ui should show step2 details on right side
     const step2 = await screen.findByRole('button', { name: /Command-2/i })
     userEvent.click(step2)
     await screen.findByText('ESW.test2')
-    await delay(10)
+
+    window.postMessage(updatedStepListWithStep3InProgress, '*')
     //as user is in follow mode, and after some time ui should show step3 details on right side as steplist progress
     await screen.findByText('ESW.test3')
   })
 
   it('should show current running step when step list is long in follow mode | ESW-501', async () => {
-    const step1To17 = new Array(17).fill('Success').map((s, index) => getStep(s, `${index + 1}`))
-
-    const stepListWithStep18InProgress: StepList = new StepList([
-      ...step1To17,
-      getStep('InFlight', '18'),
-      getStep('Pending', '19'),
-      getStep('Pending', '20')
-    ])
-
-    const stepListWithStep19InProgress: StepList = new StepList([
-      ...step1To17,
-      getStep('Success', '18'),
-      getStep('InFlight', '19'),
-      getStep('Pending', '20')
-    ])
-
-    when(sequencerServiceMock.subscribeSequencerState()).thenReturn(
-      (callback: (sequencerStateRes: SequencerStateResponse) => void) => {
-        sendEvent(callback, 'Running', stepListWithStep18InProgress)
-        sendEvent(callback, 'Running', stepListWithStep19InProgress, 400)
-        return {
-          cancel: () => undefined
-        }
-      }
-    )
+    const step1To17 = new Array(17).fill('Success')
+    const stepListWithStep18InProgress: StepStatus['_type'][] = [...step1To17, 'InFlight', 'Pending', 'Pending']
+    const stepListWithStep19InProgress: StepStatus['_type'][] = [...step1To17, 'Success', 'InFlight', 'Pending']
 
     renderWithAuth({
       ui: (
@@ -242,11 +174,12 @@ describe('sequencer details selected step', () => {
         </BrowserRouter>
       )
     })
+    window.postMessage(stepListWithStep18InProgress, '*')
     //step18 is executing, ui should show step18 details on right side
     await assertRunningStepIs(/Command-18/i, 500)
     //wait and assert for auto scroll to happen
     await waitFor(() => expect(window.scrollY).to.greaterThan(500))
-
+    window.postMessage(stepListWithStep19InProgress, '*')
     //step19 is executing, ui should show step19 details on right side
     await assertRunningStepIs(/Command-19/i, 500)
     await screen.findByText('ESW.test19')
@@ -259,4 +192,10 @@ const assertRunningStepIs = async (step: RegExp, timeout: number) => {
   await waitFor(() => expect(stepButton1.style.borderColor).to.equal('rgb(82, 196, 26)'), { timeout })
 }
 
-export const delay = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms))
+const addEventListenerForSubscribing = (callback: (sequencerStateResponse: SequencerStateResponse) => void) => {
+  window.addEventListener('message', (event) => {
+    const eventTypes: StepStatus['_type'][] = event.data
+    const steps = eventTypes.map((x, index) => getStep(x, `${index + 1}`))
+    sendEvent(callback, 'Running', new StepList(steps))
+  })
+}

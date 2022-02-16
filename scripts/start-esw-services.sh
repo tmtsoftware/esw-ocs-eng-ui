55f1192b47cdeba91e575c67e424005e82e26d7a
manish <gowardipemanish95@gmail.com>
2022-02-16 15:24:46 +0530

    #000: Manish: Update jdk to 17 for docs & add jvmopts

--- scripts/start-esw-services.sh
+++ scripts/start-esw-services.sh
@@ -3,6 +3,6 @@ ROOT="$(
     cd "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
 )"
-SEQ_SCRIPT_VERSION=2c32b04
-ESW_VERSION=0.4.0-RC1
+SEQ_SCRIPT_VERSION=fe74931
+ESW_VERSION=3ec7078
 cs launch esw-services:$ESW_VERSION -- start-eng-ui-services  --scripts-version $SEQ_SCRIPT_VERSION

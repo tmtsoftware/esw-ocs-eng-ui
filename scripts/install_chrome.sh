b8b75dc5633b17be4623ba05fac85b1931e2fdbc
Allan Brighton <Allan.Brighton@t-online.de>
2025-04-10 18:54:15 +0200

    Testing Jenkins setup

--- scripts/install_chrome.sh
+++ scripts/install_chrome.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-wget https://storage.googleapis.com/chrome-for-testing-public/135.0.7049.84/linux64/chrome-linux64.zip
-unzip chrome-linux64.zip
+if test ! -d chrome-linux64; then
+  wget https://storage.googleapis.com/chrome-for-testing-public/135.0.7049.84/linux64/chrome-linux64.zip
+  unzip chrome-linux64.zip
+fi
+
 
